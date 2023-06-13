dfc329e5bb691d0cf4b489045d14e6359c653fa9
KHUN SOE MOE AUNG <prince.sma07@gmail.com>
2023-06-12 20:56:19 -0500

    Prepare Prob 1 & Prob 2

---
+++ src/assignment8/prob1/Person.java
@@ -0,0 +1,20 @@
+package assignment8.prob1;
+
+public class Person {
+    private String lastName;
+    private String firstName;
+    private int age;
+
+    public Person(String last, String first, int a) { // constructor
+        lastName = last; firstName = first; age = a;
+    }
+
+    public String getLast() // get last name
+    {
+        return lastName;
+    }
+    @Override
+    public String toString() {
+        return "Person [lastName=" + lastName + "\n FirstName=" + firstName +
+                "\nAge=" + age + "]"; }
+}
